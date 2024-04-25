1ed2923e844212735006ec236c70e64347eadaf0
Chris Phillipson <cphillipson@pivotal.io>
2024-04-25 09:42:32 -0700

    Fix checking logic for existence of Azure credentials

--- docker/actions/azure/tanzu-runsh-setup-action/entrypoint.sh
+++ docker/actions/azure/tanzu-runsh-setup-action/entrypoint.sh
@@ -12,7 +12,7 @@
 #
 
 
-if [ x"${AZURE_SUBSCRIPTION_ID}" == "x" ] || [ x"${AZURE_AD_TENANT_ID}" == "x" ] && [ x"${AZURE_AD_CLIENT_ID}" == "x" ] || [ x"${AZURE_AD_CLIENT_SECRET}" == "x" ]; then
+if [ x"${AZURE_SUBSCRIPTION_ID}" == "x" ] || [ x"${AZURE_AD_TENANT_ID}" == "x" ] || [ x"${AZURE_AD_CLIENT_ID}" == "x" ] || [ x"${AZURE_AD_CLIENT_SECRET}" == "x" ]; then
   echo "Expected AZURE_SUBSCRIPTION_ID, AZURE_AD_TENANT_ID, AZURE_AD_CLIENT_ID, and AZURE_AD_CLIENT_SECRET enviroment variables to have been set!"
   exit 1;
 fi
